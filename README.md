10637b3e740325b1357dfedd67f1fc45dc21fa08
fengshaoyun <syfeng@vcread.com>
2017-11-25 10:08:58 +0800

    添加readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # redis-console
-  自动部署与监控redis控制台
+自动部署与监控redis控制台
+
+## 功能
+- 配置管理
+  - server管理，默认用户名密码，各server用户
+  - redis实例管理
+- 状态监控
+  - server状态监控
+  - redis监控
+
+
--- src/main/java/cn/tools3/redis/console/App.java
+++ src/main/java/cn/tools3/redis/console/App.java
@@ -1,13 +1,19 @@
 package cn.tools3.redis.console;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
+import org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(repositoryFactoryBeanClass = DataTablesRepositoryFactoryBean.class)
+@EnableAutoConfiguration
+public class App {
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(App.class);
+		app.addListeners(new ApplicationPidFileWriter("application.pid"));
+		app.run(args);
+	}
 }
