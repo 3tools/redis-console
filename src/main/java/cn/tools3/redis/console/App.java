16837eeaed29d09be7a6da265070bb06b1e9213b
fengshaoyun <syfeng@vcread.com>
2017-11-25 09:59:49 +0800

    初始化工程

---
+++ src/main/java/cn/tools3/redis/console/App.java
@@ -0,0 +1,13 @@
+package cn.tools3.redis.console;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
