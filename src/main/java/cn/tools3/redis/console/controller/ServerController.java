6e98b6986b49ceb3e56997cd32db6fa392a00d2e
yangting <ytzyangting@126.com>
2017-12-04 23:58:32 +0800

    添加layer插件，方便编写页面

--- src/main/java/cn/tools3/redis/console/controller/ServerController.java
+++ src/main/java/cn/tools3/redis/console/controller/ServerController.java
@@ -1,8 +1,8 @@
 package cn.tools3.redis.console.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-
+import cn.tools3.redis.console.domain.Server;
+import cn.tools3.redis.console.repository.ServerRepository;
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.datatables.mapping.DataTablesInput;
 import org.springframework.data.jpa.datatables.mapping.DataTablesOutput;
@@ -11,10 +11,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.fasterxml.jackson.annotation.JsonView;
-
-import cn.tools3.redis.console.domain.Server;
-import cn.tools3.redis.console.repository.ServerRepository;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/server")
@@ -27,6 +25,11 @@ public class ServerController {
 	public String list(ModelMap model) {
 		return "server/vList";
 	}
+
+    @RequestMapping(value = "/add")
+    public String add(ModelMap model) {
+        return "server/vAdd";
+    }
 
 	@JsonView(DataTablesOutput.View.class)
 	@RequestMapping(value = "/datatable")
