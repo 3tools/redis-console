97e7e8385c3260b6f81328224a3bbe482b5dd732
renhuan <renhuan>
2017-12-05 11:00:25 +0800

    add server metrics controller and test init

---
+++ src/main/java/cn/tools3/redis/console/controller/ServerMetricsController.java
@@ -0,0 +1,25 @@
+package cn.tools3.redis.console.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author :  blentle
+ * @email : blentle.huan.ren@gmail.com
+ * @time :  2017/12/5 0005
+ * @description : monitor to supervise redis server
+ * @since : 1.0
+ */
+@Controller
+@RequestMapping("/metrics")
+public class ServerMetricsController {
+    private static final Logger logger = LoggerFactory.getLogger(ServerMetricsController.class);
+
+    public ModelAndView listServers() {
+        //todo:
+        return null;
+    }
+}
