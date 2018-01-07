88246008f1a9f53ee0ad7940d9691de859394af2
fengshaoyun <fengshaoyun@192.168.18.128>
2018-01-07 09:10:17 +0800

    修改启动脚本

--- src/main/java/cn/tools3/redis/console/service/RedisService.java
+++ src/main/java/cn/tools3/redis/console/service/RedisService.java
@@ -8,13 +8,35 @@ import com.jcabi.ssh.SshByPassword;
 @Service
 public class RedisService {
 
+	private String redisFileName = "redis-4.0.1.tar";
+
+	private String appUrl = "http://localhost:10000";
+
 	/**
 	 * 部署
 	 */
 	public void deploy(String host, int port, String user, String pass) throws Exception {
-		 new Shell.Plain(new SshByPassword(host, 22, user, pass)).exec("");
+		Shell.Plain plain = new Shell.Plain(new SshByPassword(host, 22, user, pass));
+		plain.exec("mkdir ~/.redis");
+		String check = plain.exec("cd ~/.redis && ls -lrt");
+		if (!check.contains(redisFileName)) {
+			plain.exec("cd ~/.redis && wget " + appUrl + "/" + redisFileName);
+			plain.exec("cd ~/.redis && tar -xvf " + redisFileName);
+			String redisDir = redisFileName.substring(0, redisFileName.lastIndexOf("."));
+			plain.exec("cd ~/.redis/" + redisDir + " && make");
+		}
 	}
 
+	/**
+	 * 启动redis
+	 * 
+	 * @param host
+	 * @param port
+	 * @param user
+	 * @param pass
+	 * @param redisPort
+	 * @throws Exception
+	 */
 	public void start(String host, int port, String user, String pass, int redisPort) throws Exception {
 		new Shell.Plain(new SshByPassword(host, 22, user, pass))
 				.exec("screen -d -m  ~/.redis/redis-4.0.1/src/redis-server ~/.redis/" + redisPort + "/redis.conf ");
